4f398dc80dd5be098e466d46b4f279a4f317adb5
yuzuzu <suzu120782@gmail.com>
2026-08-24 22:22:58 +0900

    Update product list heading

--- app/page.tsx
+++ app/page.tsx
@@ -183,7 +183,7 @@ export default function Home() {
         </p>
 
         <h1 className="mt-1 text-lg font-bold leading-tight">
-          SHOWER OF HEART
+          SHOWER OF HEART ★
         </h1>
 
         <p className="mt-1 text-xs text-white">
